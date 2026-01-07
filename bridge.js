58c17ee11aaddba27ed2ea58638b004f4bf6ac67
TrisCC <tristancotino@gmail.com>
2026-01-07 14:30:39 +0100

    Fix assignment

--- bridge.js
+++ bridge.js
@@ -1,30 +1,35 @@
-var osc = require('node-osc');
-var io = require('socket.io')(8081);
+const osc = require('node-osc');
+const io = require('socket.io')(8081, {
+	cors: {
+		origin: "*",
+		methods: ["GET", "POST"]
+	}
+});
 
 
-var oscServer, oscClient;
+let oscServer, oscClient;
 
-var isConnected = false;
+let isConnected = false;
 
-io.sockets.on('connection', function (socket) {
+io.on('connection', function (socket) {
 	console.log('connection');
 	socket.on("config", function (obj) {
 		isConnected = true;
-    	oscServer = new osc.Server(obj.server.port, obj.server.host);
-	    oscClient = new osc.Client(obj.client.host, obj.client.port);
-	    oscClient.send('/status', socket.sessionId + ' connected');
-		oscServer.on('message', function(msg, rinfo) {
+		oscServer = new osc.Server(obj.server.port, obj.server.host);
+		oscClient = new osc.Client(obj.client.host, obj.client.port);
+		oscClient.send('/status', socket.id + ' connected');
+		oscServer.on('message', function (msg, rinfo) {
 			socket.emit("message", msg);
 		});
 		socket.emit("connected", 1);
 	});
- 	socket.on("message", function (obj) {
+	socket.on("message", function (obj) {
 		oscClient.send.apply(oscClient, obj);
-  	});
-	socket.on('disconnect', function(){
+	});
+	socket.on('disconnect', function () {
 		if (isConnected) {
-			oscServer.kill();
-			oscClient.kill();
+			oscServer.close();
+			oscClient.close();
 		}
-  	});
+	});
 });
--- p5-basic/sketch.js
+++ p5-basic/sketch.js
@@ -1,25 +1,78 @@
-var x, y;
+// Particle-based OSC emitter
+// By Tristan Cotino
+
+// This sketch creates particles that fall under gravity and bounce on the bottom of the canvas.
+// On each bounce, an OSC message is sent with the horizontal position and impact strength,
+// which can be used to trigger sound grains in an external application.
+
+var socket;
+let particles = [];
+let gravity = 0.25;
 
 function setup() {
-	createCanvas(500, 500);
-	setupOsc(12000, 3334);
+	setupOsc(8081, 3334)
+	createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-	background(0, 0, 255);
-	fill(0, 255, 0);
-	ellipse(x, y, 100, 100);
-	fill(0);
-	text("I'm p5.js", x-25, y);
+	background(20);
+
+	// Update and display particles
+	for (let i = particles.length - 1; i >= 0; i--) {
+		let p = particles[i];
+		p.velocity += gravity;
+		p.y += p.velocity;
+		p.x += p.drift;
+
+		// Bounce Logic
+		if (p.y > height) {
+			p.y = height;
+			p.velocity *= -0.7; // Dampen the bounce
+
+			// If the impact is strong enough, trigger a grain
+			if (abs(p.velocity) > 1) {
+				sendGrainOSC(p.x, abs(p.velocity));
+			}
+		}
+
+		// Remove slow/dead particles
+		if (abs(p.velocity) < 0.5 && p.y >= height) {
+			particles.splice(i, 1);
+		}
+
+		// Remove particles that go off-screen
+		if (p.x < 0 || p.x > width) {
+			particles.splice(i, 1);
+		}
+
+		fill(0, 200, 255, 255);
+		noStroke();
+		ellipse(p.x, p.y, 16, 16);
+	}
+}
+
+// Click to emit a particle "emitter"
+function mousePressed() {
+	particles.push({
+		x: mouseX,
+		y: mouseY,
+		velocity: 0,
+		drift: random(-1, 1),
+		lifespan: 255
+	});
+}
+
+function sendGrainOSC(posX, impact) {
+	// Normalize values for PlugData (0.0 to 1.0)
+	let normX = map(posX, 0, width, 0, 1);
+	let normAmp = map(impact, 0, 15, 0, 1);
+
+	sendOsc('/grain', [normX, normAmp]);
+	println("Sent OSC /grain: " + normX + ", " + normAmp);
 }
 
 function receiveOsc(address, value) {
 	console.log("received OSC: " + address + ", " + value);
-
-	if (address == '/test') {
-		x = value[0];
-		y = value[1];
-	}
 }
 
 function sendOsc(address, value) {
@@ -27,16 +80,16 @@ function sendOsc(address, value) {
 }
 
 function setupOsc(oscPortIn, oscPortOut) {
-	var socket = io.connect('http://127.0.0.1:8081', { port: 8081, rememberTransport: false });
-	socket.on('connect', function() {
+	socket = io.connect('http://127.0.0.1:8081');
+	socket.on('connect', function () {
 		socket.emit('config', {
-			server: { port: oscPortIn,  host: '127.0.0.1'},
-			client: { port: oscPortOut, host: '127.0.0.1'}
+			server: { port: oscPortIn, host: '127.0.0.1' },
+			client: { port: oscPortOut, host: '127.0.0.1' }
 		});
 	});
-	socket.on('message', function(msg) {
+	socket.on('message', function (msg) {
 		if (msg[0] == '#bundle') {
-			for (var i=2; i<msg.length; i++) {
+			for (var i = 2; i < msg.length; i++) {
 				receiveOsc(msg[i][0], msg[i].splice(1));
 			}
 		} else {
